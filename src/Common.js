ecf4e462c321be6fc6738cdb6cd773da053fc772
baboria364 <baboria364@gmail.com>
2021-08-06 14:50:21 +0530

    first app

---
+++ src/Common.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import web from "../src/images/img2.png"
+
+const Common = (props) => {
+    return (
+        <>
+            <section id="header" className="d-flex align-items-center">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-10 mx-auto  ">
+                            <div className="row">
+                                <div className="col-md-6 pt-5 pt-lg-0 order-2 orde-lg-1 mt-5 d-flex justify-content-center flex-column">
+                                    <h1 >
+                                        {props.name}
+                                        <strong className="brand-name">Web-Chat</strong>
+                                    </h1>
+                                    <h2 className="my-3">
+                                        We connect you with each other and strike up personal conversations with your friends.
+                                    </h2>
+                                    <div className="mt-3">
+                                        <NavLink to={props.visit} className="btn btn-outline-info">
+                                            {props.btname}
+                                        </NavLink>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                                    <img src={props.imgsrc} className="img-fluid animated" alt="home img"></img>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* <footer className="text-center bg-info">Created by vishal</footer> */}
+        </>
+
+    )
+}
+
+export default Common;
+
+
+
+
+
+{/* <div className="container-fluid nav_bg">
+<div className="row">
+    <div className="col-10 mx-auto  ">
+        <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+        </div>
+    </div>
+</div>
+</div> */}
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <>
+      <h1>Welcome to Log in Page</h1>
+    </>
+  
+  )
+}
+
+export default Login;
