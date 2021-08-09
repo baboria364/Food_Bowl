580ef801db2bd2edae0c41c75c61e4f834d8ae30
baboria364 <baboria364@gmail.com>
2021-08-09 17:39:13 +0530

    hungry point

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer';
 import Cards from './Components/Cards'
 import Carousels from './Components/Carousels';
